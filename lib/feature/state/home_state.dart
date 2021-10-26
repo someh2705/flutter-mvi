f80e1ae3a84c111a426d51d86c1335732a897d47
someh <someh2705@gmail.com>
2021-10-27 03:05:15 +0900

    feat: Home State

---
+++ lib/feature/state/home_state.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class HomeState {
+  HomeState._();
+
+  factory HomeState.loading() = HomeLoadingState;
+
+  factory HomeState.unload() = HomeUnloadState;
+
+  factory HomeState.complete(Image image) = HomeCompleteState;
+
+  factory HomeState.error(String error) = HomeErrorState;
+}
+
+class HomeLoadingState extends HomeState {
+  HomeLoadingState() : super._();
+}
+
+class HomeUnloadState extends HomeState {
+  HomeUnloadState() : super._();
+}
+
+class HomeErrorState extends HomeState {
+  HomeErrorState(this.error) : super._();
+
+  final String error;
+}
+
+class HomeCompleteState extends HomeState {
+  HomeCompleteState(this.image) : super._();
+
+  final Image image;
+}
